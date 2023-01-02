7ef3aa05a0b8cd3457f104a027f4fdbe357337c0
unknown <nelsonterdoo1000@gmail.com>
2023-01-03 00:37:32 +0100

    Original work

---
+++ index.js
@@ -0,0 +1,6 @@
+const arrowAnim = document.getElementById('arrowAnim');
+arrowAnim.style.position='absolute';
+arrowAnim.style.top = '150px';
+arrowAnim.style.left = '150px';
+console.log(arrowAnim.style.top);
+console.log(arrowAnim.style.left);
